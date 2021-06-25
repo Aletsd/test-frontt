22bf4f670f7649cb680126154eb184af4f519c15
Alexis Velasco <oaletsd@gmail.com>
2021-06-25 00:58:02 -0500

    Create proyect and configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        customapp : './src/customapp.jsx'
+    },
+    module :{
+        rules : [
+            {
+                test : /\.(js|jsx)$/, 
+                use : ['babel-loader']
+            }
+        ]
+    },
+    output: {
+        filename : '[name].bundle.js',
+        path : path.resolve(__dirname, 'dist')
+    }
+}
